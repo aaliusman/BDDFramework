76236f6e46f926c0c9198e497c5e1e9bcf1c07c4
aaliusman <aali.usman83@gmail.com>
2023-02-28 16:51:32 -0500

    added tests for state farm

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -10,29 +10,55 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 public class LoginPage {
 
 
-   WebDriver driver = null;
+    WebDriver driver = null;
     String expectedErrorMessage = "Incorrect format Enter a valid 5-digit US ZIP Code.";
+
+    public Properties getDataFromPropertyFile(String fileName) throws IOException {
+        FileInputStream fis = null;
+        Properties prop = null;
+        try {
+            fis = new FileInputStream(fileName);
+            prop = new Properties();
+            prop.load(fis);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            fis.close();
+        }
+        return prop;
+    }
 
     
 
     @Test
-    public void validateLoginPage() throws InterruptedException {
+    public void validateLoginPage() throws InterruptedException, IOException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.statefarm.com");
         driver.findElement(By.xpath("//*[@id='quote-main-zip-code-input']")).sendKeys("19047");
-        driver.findElement(By.xpath("//*[@id='oneX-header']/nav/section[3]/div[1]/div[1]/ul/li[1]/div/button")).click();
+        driver.findElement(By.xpath("//button[@data-for='insurance']")).click();
         List<WebElement> links = driver.findElements(By.xpath("//ul[@class='-oneX-subMenu-items -oneX-hidden  ']/li/a"));
 
         for (int i = 0;i<links.size();i++) {
                 System.out.println(links.get(i).getText());
         }
         Thread.sleep(2000);
+
+        Properties prop = getDataFromPropertyFile("config.properties");
+        System.out.println("username: "+ prop.getProperty("name"));
+
 
         int lengthOfZipCode = driver.findElement(By.xpath("//*[@id='quote-main-zip-code-input']")).getAttribute("value").length();
 //        Assert.assertEquals(lengthOfZipCode, 5);
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,68 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("--Hello World=--");
+    WebDriver driver = null;
+    String expectedZipCodeErrorMessage = "Enter a valid 5-digit ZIP Code.";
+
+    @Test
+    public void stateFarmHomePage() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();// Gecko Driver, IE iedriver, safari driver
+        driver.get("https://www.statefarm.com");
+        driver.findElement(By.id("quote-main-zip-code-input")).sendKeys("19047");
+        int lengthOfZipCode = driver.findElement(By.id("quote-main-zip-code-input")).getAttribute("value").length();
+        System.out.println(lengthOfZipCode);
+        Assert.assertEquals(lengthOfZipCode, 5);
+
+    }
+
+    @Test
+    public void validateIfZipCodeTakesMoreThan5Integer() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();// Gecko Driver, IE iedriver, safari driver
+        driver.get("https://www.statefarm.com");
+        driver.findElement(By.id("quote-main-zip-code-input")).sendKeys("1904745345345");
+        int lengthOfZipCode = driver.findElement(By.id("quote-main-zip-code-input")).getAttribute("value").length();
+        System.out.println(lengthOfZipCode);
+        Assert.assertEquals(lengthOfZipCode, 5);
+    }
+
+    @Test
+    public void validateIfZipCodeFieldTakesCharacters() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();// Gecko Driver, IE iedriver, safari driver
+        driver.get("https://www.statefarm.com");
+        driver.findElement(By.id("quote-main-zip-code-input")).sendKeys("abcedf");
+        int lengthOfZipCode = driver.findElement(By.id("quote-main-zip-code-input")).getAttribute("value").length();
+        System.out.println(lengthOfZipCode);
+        Assert.assertEquals(lengthOfZipCode, 0);
+    }
+
+    @Test
+    public void validateZipCodeErrorMessage() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();// Gecko Driver, IE iedriver, safari driver
+        driver.get("https://www.statefarm.com");
+        driver.findElement(By.id("quote-main-zip-code-input")).sendKeys("00098", Keys.ENTER);
+        Thread.sleep(2000);
+        String errorMessage = driver.findElement(By.cssSelector("#quote-error-alert")).getText();
+        System.out.println(errorMessage);
+        Assert.assertEquals(errorMessage, expectedZipCodeErrorMessage);
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit(); // driver.close();
     }
 }
